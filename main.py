b03865a4f4c095c23c6bb2e0f948a35ab62ef500
Edric Kusuma <edric.kusuma@gmail.com>
2021-01-30 10:22:48 -0800

    initial hello world

---
+++ main.py
@@ -0,0 +1,17 @@
+import discord
+import os
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('BEEP BEEP. Logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send('Horayo!')
+
+client.run(os.getenv('TOKEN'))
